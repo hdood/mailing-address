808d5878ce36c0e6f68ccd207eb750f00ec24ab9
hdood <86691012+hdood@users.noreply.github.com>
2023-01-18 20:31:59 +0100

    Update handleResponse.js

--- src/handleResponse.js
+++ src/handleResponse.js
@@ -19,15 +19,17 @@ export default function handleResponse(input, response){
         showError(message); 
         return;
     }
+    let res = {}; 
 
-    const res = {
-        address : xmlDoc.getElementsByTagName("Address1")[0].innerHTML,
-        address2 : xmlDoc.getElementsByTagName("Address2")[0].innerHTML,
-        state : xmlDoc.getElementsByTagName("State")[0].innerHTML,
-        city : xmlDoc.getElementsByTagName("City")[0].innerHTML,
-        zip : xmlDoc.getElementsByTagName("Zip5")[0].innerHTML,
+    if(xmlDoc.getElementsByTagName("Address1")[0]){
+        res.Address_Line_1 = xmlDoc.getElementsByTagName("Address1")[0].innerHTML; 
     }
-
+    if(xmlDoc.getElementsByTagName("Address2")[0]){
+        res.Address_Line_2 = xmlDoc.getElementsByTagName("Address2")[0].innerHTML; 
+    }
+    res.State = xmlDoc.getElementsByTagName("State")[0].innerHTML;
+    res.City = xmlDoc.getElementsByTagName("City")[0].innerHTML;
+    res.Zip = xmlDoc.getElementsByTagName("Zip5")[0].innerHTML; 
 
     // ask the user if he wants  to keep the adress he entred or the normalized one
 
